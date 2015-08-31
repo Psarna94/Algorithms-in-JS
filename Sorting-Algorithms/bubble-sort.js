fab3399bfdd480a60064b59ff4baad14ec929c05
Pranu Sarna <pranusarna@gmail.com>
2015-08-31 18:06:41 +0530

    Added bubble sort

---
+++ Sorting-Algorithms/bubble-sort.js
@@ -0,0 +1,21 @@
+var arr=[2,4,7,1,5,3];
+
+
+function bubbleSort(a)
+{
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
+
+bubbleSort(arr);
+console.log(arr);
