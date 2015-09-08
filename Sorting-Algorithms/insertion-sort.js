2adbd61d5364fefb8f22f67103f1ac115ce2f023
Pranu Sarna <pranusarna@gmail.com>
2015-09-08 13:11:54 +0530

    updating master branch

--- Sorting-Algorithms/insertion-sort.js
+++ Sorting-Algorithms/insertion-sort.js
@@ -4,7 +4,7 @@ function insertionSort(arr){
   for(var j=1; j<arr.length; j++){
     var key = arr[j];
     var i = j -1;
-    while(i>-1 && arr[i] > key){
+    while(i>-1 && arr[i]>key){
       arr[i+1] = arr[i];
       i--;
     }
